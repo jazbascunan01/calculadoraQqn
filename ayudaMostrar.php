c559735d5e6bef7dba0425b6e8571b116513d793
mmartine2008 <mariano.andres.martinez@gmail.com>
2023-04-12 17:56:38 -0300

    Separacion en funciones·

---
+++ ayudaMostrar.php
@@ -0,0 +1,24 @@
+<?php
+
+    function encabezado() {
+        $encabezado = "<!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset='utf-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <title>Calculadora</title>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+        </head>
+        <body>
+            <h1>Calculadora</h1>";
+
+        return $encabezado;
+    }
+
+    function pie() {
+        $pie = "<a href=\"index.php\">Volver</a> 
+        </body>
+        </html>";
+
+        return $pie;
+    }    
---
+++ matematica.php
@@ -0,0 +1,17 @@
+<?php
+
+function suma($a, $b) {
+    return $a + $b;
+}
+
+function multiplica($a, $b) {
+    return $a * $b;
+}
+
+function resta($a, $b) {
+    return $a - $b;
+}
+
+function divide($a, $b) {
+    return $a / $b;
+}
