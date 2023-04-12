08aa5a30e798ac023f95736bd5637cbfea8c7ebd
mmartine2008 <mariano.andres.martinez@gmail.com>
2023-04-12 18:24:20 -0300

    Acerca con nombre

--- acerca.php
+++ acerca.php
@@ -1,7 +1,14 @@
 <?php
 require_once('ayudaMostrar.php');
 
+$contenido = '';
+if (isset($_REQUEST['nombre'])) {
+    $nombre = $_REQUEST['nombre'];
+    $contenido = "<p>Por: $nombre</p>";
+}
+
 $contenido = encabezado() .
+        $contenido.
         "<p>TUDAI - QQn 2023</p>".pie();
 
 echo $contenido;
